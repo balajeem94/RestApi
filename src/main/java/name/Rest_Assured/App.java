bd1ed28ce29ac3f992c579bc9092ee477ca849e6
balajeem94 <balachennai2@gmail.com>
2022-01-04 09:58:19 +0530

    first commit

---
+++ src/main/java/name/Rest_Assured/App.java
@@ -0,0 +1,13 @@
+package name.Rest_Assured;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/name/Rest_Assured/firsttest.java
@@ -0,0 +1,15 @@
+package name.Rest_Assured;
+import static io.restassured.RestAssured.*;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+
+public class firsttest {
+	@Test
+	public void sample() {
+		RestAssured.baseURI="http://demo.guru99.com";
+
+		given().when().get("/V4/sinkministatement.php").then().log().all().assertThat().statusCode(200);
+}
+}
